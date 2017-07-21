df5ffc11bfc2a412d46faa5e07d19e170b1c3103
Tomek Sochacki <tomasz.sochacki@drogimex.pl>
2017-07-22 01:19:34 +0200

    Update README and mocha tests

--- README.md
+++ README.md
@@ -1,21 +1,40 @@
 # Konwertowanie kwoty liczbowej do postaci słownej
 
-Skrypt umożliwiający przestawienie kwot w postaci słownej w języku polskim. Funkcja pobiera kwotę jako wartość liczbową lub ciąg znakowy i zwraca jej podstać słowną zgodną ze wskazanym formatem wyświetlania (lub wg formatu domyślnego).
+Skrypt umożliwiający przedstawienie kwot w postaci słownej (w języku polskim). Funkcja pobiera kwotę jako wartość liczbową lub ciąg znakowy i zwraca jej podstać słowną zgodną ze wskazanym formatem wyświetlania (lub wg formatu domyślnego).
 
-### Sposób użycia funkcji priceToWords
+###Instalacja i uruchomienie modułu
+Aby pobrać moduł npm należy użyć komendy:
+```
+npm install --save-dev price-to-polish-words
+```
+
+### Sposób użycia
 Na początku należy stworzyć nowy obiekt PriceToWords, przekazując do konstruktora kwotę, którą chcemy przekonwertować do postaci słownej lub sformatować wg ustaonych zasad.
 ```
-let a = new PriceToWords(120.50);
-    b = new PriceToWords("120.50");
-    c = new PriceToWords("120.50zł");
-    d = new PriceToWords("120zł");
-    e = new PriceToWords("120.540,79zł");
+const PriceToPolishWords = require('price-to-polish-words');
+
+const a = new PriceToPolishWords(120.50);
+const b = new PriceToPolishWords("120.50");
+const c = new PriceToPolishWords("120");
+const d = new PriceToPolishWords(120);
+const e = new PriceToPolishWords("120,540.50");
 ```
-Wartość przekazana jako argument dla konstruktora PriceToWords musi być wartością, która będzie mogła zostać poprawnie przekonwertowana do kwoty liczbowej (konwertowaie odbywa się przy użyciu wyrażeń regularnych, które dopuszczają podawanie kwot z końcówkami "zł", "gr" oraz kwot z użyciem separatorów tysięcznych w formie kropki lub przecinka, przy czym ostatni separator zakłada się, że określa liczbę groszy.
+Wartość przekazana jako argument dla konstruktora PriceToWords musi być wartością, która będzie mogła zostać poprawnie przekonwertowana do kwoty liczbowej. Konwertowaie odbywa się przy użyciu wyrażeń regularnych, które dopuszczają podawanie kwot z użyciem separatorów tysięcznych w formie kropki lub przecinka, przy czym ostatni separator traktowany jest jako separacja groszy.
+
+Konstruktor przyjmuje również drugi, opcjonalny argument, którym jest komunikat błędu, zwracany w przypadku podania jako kwoty wartośi, której nie da się w prawidłowy sposób przekonwertować na wartość liczbową.
+```
+const priceUser = new PriceToPolishWords('xxx', 'Ups, błędna kwota!');
+const priceDefault = new PriceToPolishWords('xxx');
+
+priceUser.getPrice();    //'Ups, błędna kwota!'
+priceDefault.getPrice(); //'Błędna kwota!'
+```
+W przypadku braku druiego argumentu w razie wystąpienia błędu zwrócony zostanie komunikat domyślny, czyli "Błędna kwota!".
+
 
 Kolejnym etapem jest wywołanie metody getPrice, która pobiera jeden, opcjonalny argument wskazujący na format konwertowania kwoty:
 ```
-let price = new PriceToWords("12,50zł");
+const price = new PriceToPolishWords("12,50");
 
 price.getPrice(); 
 //dwanaście złotych pięćdziesiąt groszy
@@ -23,6 +42,7 @@ price.getPrice();
 price.getPrice('c');
 //dwanaście zł 50/100
 ```
+Format konwertowania musi zostać przekazany jako wartość typu string w formie formatu prefediniowanego lub formatu użytkownika.
 
 ### Formaty konwertowania stosowane w metodzie getPrice
 W przypadku pominięcia parametru w wywołaniu metody getPrice użyty zostanie forma domyślny. Funkcja umożliwia określenie własnego formatu na podstawie dostępnych elementów składowych lub określenie jednego z formatów predefiniowanych.
@@ -32,47 +52,32 @@ W przypadku pominięcia parametru w wywołaniu metody getPrice użyty zostanie f
 price.getPrice('b'); 
 //w przypadku formaty predefiniowanego
 
-price.getPrice('[zl_value_words] [zl_abbrev]');
+price.getPrice('zl-words zl');
 //w przypadku określania własnych reguł konwersji
 ```
-Własne reguły podaje się z jako ciąg znakowy, zawierający poszczególne elementy zapisywane w nawiasach kwadratowych w notacji z podkreśleniami dolnymi. Wszystkie znaki nie będące poprawnym elementem formatowania [...] zostaną zignorowane.
+Własne reguły podaje się z jako ciąg znakowy, zawierający poszczególne elementy oddzielone spacją. W przypadku podania błędnego stylu formatowania zwrócony zostanie komunikat "Invalid format!".
 
 ***Dopuszczalne elementy:***
-Kwota testowa: 120,50zł
+Kwota testowa: 125,50
 ```
-[zl_value_number]   === "120"
-[gr_value_number]   === "50"
-[full_value_number] === "120,50"
-
-[zl_value_words] === "sto dwadzieścia"
-[gr_value_words] === "pięćdziesiąt"
-
-[zl_abbrev] === "zł"
-[gr_abbrev] === "gr"
-[gr_short]  === "50/100"
-[zl_full] === "złoty" / "złote" / "złotych"
-[gr_full] === "grosz" / "grosze" / "groszy"
+zl-number === "125"
+gr-number === "50"
+zl-words === "sto dwadzieścia pięć"
+gr-words === "pięćdziesiąt"
+zl === "zł"
+gr === "gr"
+gr-short === "50/100"
+zl-full === "złoty" || "złote"  || "złotych"
+gr-full === "grosz" || "grosze" || "groszy"
 ```
 
 ***Formaty predefniowane:***
 Formaty można podawać w zapisie "a" lub "A".
 ```
-'A' === '[zl_value_words] [zl_full] [gr_value_words] [gr_full]'
-        //sto dwadzieścia pięć złotych pięćdziesiąt groszy
-
-'B' === '[zl_value_words] [zl_abbrev] [gr_value_words] [gr_abrev]'
-        //sto dwadzieścia pięć zł pięćdziesiąt gr
-        
-'C' === '[zl_value_words] [zl_abbrev] [gr_short]'
-        //sto dwadzieścia pięć zł 50/100
-
-'D' === '[zl_value_number] [zl_abbrev] [gr_value_number] [gr_abbrev]'
-        //120 zł 50 gr
-
-'E' === '[full_value_number] [zl_abbrev]'
-        //120,50 zł
+typeA: 'zl-words zl-full gr-words gr-full', //sto dwadzieścia pięć złotych pięćdziesiąt groszy
+typeB: 'zl-words zl gr-words gr',           //sto dwadzieścia pięć zł pięćdziesiąt gr
+typeC: 'zl-words zl gr-short',              //sto dwadzieścia pięć zł 50/100
+typeD: 'zl-number zl gr-number gr'          //125 zł 50 gr
 ```
 
-W przypadku podania błędnej kwoty przy tworzeniu obiektu PriceToWords zwrócona zostanie wartość "Podana kwota jest nieprawidłowa!".
-
-***Funkcja pozwala na konwertowanie kwot od zera zł do 999.999.999.999,99 (jeden trylion - 0,01).***
+***Funkcja pozwala na konwertowanie kwot od zera zł do 999,999,999,999.99***
--- test.js
+++ test.js
@@ -1,89 +1,98 @@
-const assert = require('chai').assert;
-const PriceToPolishWords = require('./priceToPolishWords');
+const assert = require( 'chai' ).assert;
+const PriceToPolishWords = require( './priceToPolishWords' );
 
-describe('Test of price: 125,50', () => {
-    const price = new PriceToPolishWords('125,50');
-    it('Test of default price format', () => {
-        assert.equal(price.getPrice(), 'sto dwadzieścia pięć złotych pięćdziesiąt groszy');
-        assert.equal(price.getPrice(undefined), 'sto dwadzieścia pięć złotych pięćdziesiąt groszy');
-    });
-    it('Test for basic formats', () => {
-        assert.equal(price.getPrice('zl-number'), '125');
-        assert.equal(price.getPrice('gr-number'), '50');
-        assert.equal(price.getPrice('zl-words'), 'sto dwadzieścia pięć');
-        assert.equal(price.getPrice('gr-words'), 'pięćdziesiąt');
-        assert.equal(price.getPrice('zl'), 'zł');
-        assert.equal(price.getPrice('gr'), 'gr');
-        assert.equal(price.getPrice('gr-short'), '50/100');
-        assert.equal(price.getPrice('zl-full'), 'złotych');
-        assert.equal(price.getPrice('gr-full'), 'groszy');
-    });
-    it('Test of predefined price formats', () => {
-        assert.equal(price.getPrice('a'), 'sto dwadzieścia pięć złotych pięćdziesiąt groszy');
-        assert.equal(price.getPrice('A'), 'sto dwadzieścia pięć złotych pięćdziesiąt groszy');
-        assert.equal(price.getPrice('b'), 'sto dwadzieścia pięć zł pięćdziesiąt gr');
-        assert.equal(price.getPrice('c'), 'sto dwadzieścia pięć zł 50/100');
-        assert.equal(price.getPrice('d'), '125 zł 50 gr');
-    });
-    it('Test of valid user defined formats', () => {
-        assert.equal(price.getPrice('zl-words zl-full'), 'sto dwadzieścia pięć złotych');
-        assert.equal(price.getPrice('gr-words gr-full'), 'pięćdziesiąt groszy');
-        assert.equal(price.getPrice('zl-words zl gr-words gr'), 'sto dwadzieścia pięć zł pięćdziesiąt gr');
-    });
-    it('Test of invalid user defined formats', () => {
-        assert.equal(price.getPrice('zl-words xxx zl-full'), 'Invalid format!');
-        assert.equal(price.getPrice('xxx'), 'Invalid format!');
-        assert.equal(price.getPrice(555), 'Invalid format!');
-        assert.equal(price.getPrice([]), 'Invalid format!');
-        assert.equal(price.getPrice(['xxx']), 'Invalid format!');
-        assert.equal(price.getPrice({rule: 'xxx'}), 'Invalid format!');
-    });
-});
+describe( 'Test of price: 125,50', () => {
+    const price = new PriceToPolishWords( '125,50' );
+    it( 'Test of default price format', () => {
+        assert.equal( price.getPrice(), 'sto dwadzieścia pięć złotych pięćdziesiąt groszy' );
+        assert.equal( price.getPrice( undefined ), 'sto dwadzieścia pięć złotych pięćdziesiąt groszy' );
+    } );
+    it( 'Test for basic formats', () => {
+        assert.equal( price.getPrice( 'zl-number' ), '125' );
+        assert.equal( price.getPrice( 'gr-number' ), '50' );
+        assert.equal( price.getPrice( 'zl-words' ), 'sto dwadzieścia pięć' );
+        assert.equal( price.getPrice( 'gr-words' ), 'pięćdziesiąt' );
+        assert.equal( price.getPrice( 'zl' ), 'zł' );
+        assert.equal( price.getPrice( 'gr' ), 'gr' );
+        assert.equal( price.getPrice( 'gr-short' ), '50/100' );
+        assert.equal( price.getPrice( 'zl-full' ), 'złotych' );
+        assert.equal( price.getPrice( 'gr-full' ), 'groszy' );
+    } );
+    it( 'Test of predefined price formats', () => {
+        assert.equal( price.getPrice( 'a' ), 'sto dwadzieścia pięć złotych pięćdziesiąt groszy' );
+        assert.equal( price.getPrice( 'A' ), 'sto dwadzieścia pięć złotych pięćdziesiąt groszy' );
+        assert.equal( price.getPrice( 'b' ), 'sto dwadzieścia pięć zł pięćdziesiąt gr' );
+        assert.equal( price.getPrice( 'c' ), 'sto dwadzieścia pięć zł 50/100' );
+        assert.equal( price.getPrice( 'd' ), '125 zł 50 gr' );
+    } );
+    it( 'Test of valid user defined formats', () => {
+        assert.equal( price.getPrice( 'zl-words zl-full' ), 'sto dwadzieścia pięć złotych' );
+        assert.equal( price.getPrice( 'gr-words gr-full' ), 'pięćdziesiąt groszy' );
+        assert.equal( price.getPrice( 'zl-words zl gr-words gr' ), 'sto dwadzieścia pięć zł pięćdziesiąt gr' );
+    } );
+    it( 'Test of invalid user defined formats', () => {
+        assert.equal( price.getPrice( 'zl-words xxx zl-full' ), 'Invalid format!' );
+        assert.equal( price.getPrice( 'xxx' ), 'Invalid format!' );
+        assert.equal( price.getPrice( 555 ), 'Invalid format!' );
+        assert.equal( price.getPrice( [] ), 'Invalid format!' );
+        assert.equal( price.getPrice( ['xxx'] ), 'Invalid format!' );
+        assert.equal( price.getPrice( {rule: 'xxx'} ), 'Invalid format!' );
+    } );
+} );
 
-describe('Test for round "grosze"', () => {
+describe( 'Test for round "grosze"', () => {
     const format = 'zl-words zl gr-words gr';
     
-    it('Zlote and grosze',  () => {
-        assert.equal((new PriceToPolishWords('5,00')).getPrice(format), 'pięć zł zero gr');
-        assert.equal((new PriceToPolishWords('5,05')).getPrice(format), 'pięć zł pięć gr');
-        assert.equal((new PriceToPolishWords('5,051')).getPrice(format), 'pięć zł pięć gr');
-        assert.equal((new PriceToPolishWords('5,055')).getPrice(format), 'pięć zł sześć gr');
-        assert.equal((new PriceToPolishWords('5,001')).getPrice(format), 'pięć zł zero gr');
-        assert.equal((new PriceToPolishWords('5,005')).getPrice(format), 'pięć zł jeden gr');
-        assert.equal((new PriceToPolishWords('5,25')).getPrice(format), 'pięć zł dwadzieścia pięć gr');
-        assert.equal((new PriceToPolishWords('5,252')).getPrice(format), 'pięć zł dwadzieścia pięć gr');
-        assert.equal((new PriceToPolishWords('5,255')).getPrice(format), 'pięć zł dwadzieścia sześć gr');
-    });
-    it('Only "grosze"', () => {
-        assert.equal((new PriceToPolishWords('0,05')).getPrice(format), 'zero zł pięć gr');
-        assert.equal((new PriceToPolishWords(',05')).getPrice(format), 'zero zł pięć gr');
-    });
-});
+    it( 'Zlote and grosze',  () => {
+        assert.equal( ( new PriceToPolishWords( '5,00' ) ).getPrice( format ), 'pięć zł zero gr' );
+        assert.equal( ( new PriceToPolishWords( '5,05' ) ).getPrice( format ), 'pięć zł pięć gr' );
+        assert.equal( ( new PriceToPolishWords( '5,051' ) ).getPrice( format ), 'pięć zł pięć gr' );
+        assert.equal( ( new PriceToPolishWords( '5,055' ) ).getPrice( format ), 'pięć zł sześć gr' );
+        assert.equal( ( new PriceToPolishWords( '5,001' ) ).getPrice( format ), 'pięć zł zero gr' );
+        assert.equal( ( new PriceToPolishWords( '5,005' ) ).getPrice( format ), 'pięć zł jeden gr' );
+        assert.equal( ( new PriceToPolishWords( '5,25' ) ).getPrice( format ), 'pięć zł dwadzieścia pięć gr' );
+        assert.equal( ( new PriceToPolishWords( '5,252' ) ).getPrice( format ), 'pięć zł dwadzieścia pięć gr' );
+        assert.equal( ( new PriceToPolishWords( '5,255' ) ).getPrice( format ), 'pięć zł dwadzieścia sześć gr' );
+    } );
+    it( 'Only "grosze"', () => {
+        assert.equal( ( new PriceToPolishWords( '0,05' ) ).getPrice( format ), 'zero zł pięć gr' );
+        assert.equal( ( new PriceToPolishWords( ',05' ) ).getPrice( format ), 'zero zł pięć gr' );
+    } );
+} );
 
-describe('Test of max alowed pruice', () => {
+describe( 'Separators between zl and gr', () => {
+    it( 'Comma separator', () => {
+        assert.equal( ( new PriceToPolishWords( '5,00' ) ).getPrice(), 'pięć złotych zero groszy' );
+    } );
+    it( 'Dot separator', () => {
+        assert.equal( ( new PriceToPolishWords( '5.00' ) ).getPrice(), 'pięć złotych zero groszy' );
+    } );
+} );
+
+describe( 'Test of max alowed pruice', () => {
     const format = 'zl-words zl gr-words gr';
     const d = 'dziewięćset dziewięćdziesiąt dziewięć';
     const expected = `${d} miliardów ${d} milionów ${d} tysięcy ${d} zł dziewięćdziesiąt dziewięć gr`;
     
-    it('Test of 999 999 999 999,99zł', () => {
-        assert.equal((new PriceToPolishWords('999 999 999 999,99')).getPrice(format), expected);
-        assert.equal((new PriceToPolishWords('999,999,999,999.99')).getPrice(format), expected);
-        assert.equal((new PriceToPolishWords('999999999999,99')).getPrice(format), expected);
-        assert.equal((new PriceToPolishWords('999999999999.99')).getPrice(format), expected);
-    });
-});
+    it( 'Test of 999 999 999 999,99zł', () => {
+        assert.equal( ( new PriceToPolishWords( '999 999 999 999,99' ) ).getPrice( format ), expected );
+        assert.equal( ( new PriceToPolishWords( '999,999,999,999.99' ) ).getPrice( format ), expected );
+        assert.equal( ( new PriceToPolishWords( '999999999999,99' ) ).getPrice( format ), expected );
+        assert.equal( ( new PriceToPolishWords( '999999999999.99' ) ).getPrice( format ), expected );
+    } );
+} );
 
-describe('Test of invalid price and error message', () => {
+describe( 'Test of invalid price and error message', () => {
     //default error message: Błędna kwota!
-    it('Test for default error message', () => {
-        assert.equal((new PriceToPolishWords('xxx')).getPrice(), 'Błędna kwota!');
-    });
-    it('Test for user error message getting as string', () => {
-        assert.equal((new PriceToPolishWords('xxx', 'user message')).getPrice(), 'user message');
-    });
-    it('Test for invalid user error message - not string', () => {
-        assert.equal((new PriceToPolishWords('xxx', 55)).getPrice(), 'Błędna kwota!');
-        assert.equal((new PriceToPolishWords('xxx', [])).getPrice(), 'Błędna kwota!');
-        assert.equal((new PriceToPolishWords('xxx', {})).getPrice(), 'Błędna kwota!');
-    });
-});
+    it( 'Test for default error message', () => {
+        assert.equal( ( new PriceToPolishWords( 'xxx' ) ).getPrice(), 'Błędna kwota!' );
+    } );
+    it( 'Test for user error message getting as string', () => {
+        assert.equal( ( new PriceToPolishWords( 'xxx', 'user message' ) ).getPrice(), 'user message' );
+    } );
+    it( 'Test for invalid user error message - not string', () => {
+        assert.equal( ( new PriceToPolishWords( 'xxx', 55 ) ).getPrice(), 'Błędna kwota!' );
+        assert.equal( ( new PriceToPolishWords( 'xxx', [] ) ).getPrice(), 'Błędna kwota!' );
+        assert.equal( ( new PriceToPolishWords( 'xxx', {} ) ).getPrice(), 'Błędna kwota!' );
+    } );
+} );
